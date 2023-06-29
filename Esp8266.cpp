416a7c4f8a1f6dfd8d8b87f77a2528aa0567ce1f
SrAlegre <100769860+SrAlegre@users.noreply.github.com>
2023-06-29 10:48:57 -0300

    Atualizado documentação do code do esp8266

--- Esp8266.cpp
+++ Esp8266.cpp
@@ -1,10 +1,28 @@
+/*
+  sensor de nivel usando o esp8266
+  Nome : Yuri Martins Alegre
+
+  OBS: As versões das lib's estão desatualizado devido a falta de compatibilidade delas trabalhando em conjunto.
+  Abaixo estão comentadas as versões de cada lib que foi encontrada problema, as que não possuiem versão ao lado é correspondente a mais recente
+
+  Instruções de instações:
+  
+   1) Para instalar a lib do esp8266 é posivel atraves do caminho File->Preferences . Cola o link abaixo em Additional Boards manager URLs.
+
+     http://arduino.esp8266.com/stable/package_esp8266com_index.json
+   
+   2) Agora para instalar a Lib do Arduino Json e PubSubClient, vamos acessar Tools->LiberyManger, pesquise por ArduinoJson instale, em seguida busque por PubSubClient(Versão publicada por Nick o'Leary)
+
+*/
+
+
 //Versão da lib do esp8622 : 2.6
 
 
 
 #include <ESP8266WiFi.h>
 #include <PubSubClient.h>
-#include <ArduinoJson.h>  // 6.14.1
+#include <ArduinoJson.h>  // ver. 6.14.1
 #include <Wire.h>
 #include <LiquidCrystal_I2C.h>
 
@@ -14,19 +32,19 @@
 
 
 // WiFi
-const char *ssid = "Yuri";  // Enter your WiFi name
-const char *password = "cachorro";    // Enter WiFi password
+const char *ssid = "LabTeC - Profs";  // Nome do SSID (Nome da rede Wi-Fi, Lembre de usar caracter por caracter, incluindo os espaços)
+const char *password = "";    // Senha da rede citado
 
 // MQTT Broker
-const char *mqtt_broker = "0.tcp.sa.ngrok.io";
+const char *mqtt_broker = "0.tcp.sa.ngrok.io"; // o endereçõ do broker, nesse caso como estamos hospedando no Ngrok , temos esse caminho
 
-String _topic = "sensor/" + WiFi.macAddress() + "/out"; 
+String _topic = "sensor/" + WiFi.macAddress() + "/out";  // o comando WiFi.macAddress() nos da o MAC do esp, que esta sendo usado como identificação do mesmo, String_topic sera nosso topico criado no broker
 
 
 const char *topic = _topic.c_str(); 
 const char *mqtt_username = "";
 const char *mqtt_password = "";
-const int mqtt_port = 10224;
+const int mqtt_port = 18305; //A porta para o esp acessar para conectar ao broker
 
 
 WiFiClient espClient;
@@ -52,18 +70,17 @@ void setup() {
   lcd.init();       // INICIA A COMUNICAÇÃO COM O DISPLAY
   lcd.backlight();  // LIGA A ILUMINAÇÃO DO DISPLAY
 
-  // Set software serial baud to 115200;
+  
   Serial.begin(115200);
-  // connecting to a WiFi network
-  WiFi.begin(ssid, password);
+
+  WiFi.begin(ssid, password); 
   while (WiFi.status() != WL_CONNECTED) {
     delay(500);
     Serial.println("Connecting to WiFi..");
   }
   Serial.println("Connected to the WiFi network");
 
-  //connecting to a mqtt broker
-
+//Acessar o broker
   client.setServer(mqtt_broker, mqtt_port);
   client.setCallback(callback);
   while (!client.connected()) {
@@ -78,12 +95,8 @@ void setup() {
       delay(2000);
     }
   }
-  // publish and subscribe
-  // client.publish(topic, "hello Yuri);
-
-  // client.subscribe(topic);
+  
 }
- //topic = &String(WiFi.macAddress());
 
 void callback(char *topic, byte *payload, unsigned int length) {
   Serial.print("Message arrived in topic: ");
@@ -100,16 +113,11 @@ void loop() {
   client.loop();
 
   char buffer[256];
-  float Dados = LeituraSensor();
+  float Dados = LeituraSensor(); //recebemos o dado lido pelo sensor
   char dado_string[8];
   dtostrf(Dados, 5, 2, dado_string);
 
-  Serial.print("[OLD] ESP32 Board MAC Address:  ");
-  Serial.println(WiFi.macAddress());
-
-
-
-  StaticJsonDocument<96> doc;
+  StaticJsonDocument<96> doc; //Abrimos um Arquivo estilo json para colocar os dados
 
   String Mac = String(WiFi.macAddress());
 
@@ -117,21 +125,19 @@ void loop() {
   doc["version"] = VERSION;
   doc["level"] = Dados;
   
-  serializeJson(doc, buffer);
-  //serializeJson(doc, Serial);
+  serializeJson(doc, buffer); //fizemos uma json com os dados carregados
   Serial.println();
 
 
 
-  client.publish(topic, buffer);
-  client.subscribe(topic);
+  client.publish(topic, buffer); // publicamos o json no broker
+  client.subscribe(topic); 
   delay(5000);
 }
 
 
 float LeituraSensor() {
 
-//REVER DADOS DE ENTRADAS E SAIDA , E FAZER REGRASSAO LINEAR
 
   val = analogRead(analogPin);
   val = (float)val * 5 / 1023;
@@ -146,7 +152,8 @@ float LeituraSensor() {
   lcd.setCursor(0, 3);
   //lcd.print("atualizando 3s");
   lcd.print("- Metros:");
-  metros = val / 0.83;  //Conferir parametros
+  metros = 0.0119*val - 0.2449; //Esse é o algoritmo que fizemos baseado nos dados que obtemos de forma "analogica", anotando os valores das tensões medidadas conforme aumentamos a profundidada do sensor, então essa função não é puramente linear
+  
   lcd.print(metros);
 
 
